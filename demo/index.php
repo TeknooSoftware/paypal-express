ba5635c31a55651f30fa3a75f45b5e136966b679
Richard Deloge <richarddeloge@gmail.com>
2023-04-06 08:38:17 +0200

    Fix license header

--- demo/index.php
+++ demo/index.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * LICENSE
  *
  * This source file is subject to the MIT license
- * that are bundled with this package in the folder licences
+ * it is available in LICENSE file at the root of this package
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to richard@teknoo.software so we can send you a copy immediately.
