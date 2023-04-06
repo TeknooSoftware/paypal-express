b917a52c857b525ee0215224c7f4db981a773c08
Richard Deloge <richarddeloge@gmail.com>
2023-04-06 08:26:15 +0200

    Fix license header

--- src/Paypal/Service/ExpressCheckout.php
+++ src/Paypal/Service/ExpressCheckout.php
@@ -5,8 +5,8 @@
  *
  * LICENSE
  *
- * This source file is subject to the MIT license and the version 3 of the GPL3
- * license that are bundled with this package in the folder licences
+ * This source file is subject to the MIT license
+ * that are bundled with this package in the folder licences
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to richard@teknoo.software so we can send you a copy immediately.
